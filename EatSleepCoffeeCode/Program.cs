309aa4c373a3aef48c8a708d0021d78ebc07cc72
Matthew Slone <Matthew.Slone@outlook.com>
2018-08-23 12:35:06 -0400

    initial creation. Display the coding cycle and the first phase of that cycle

--- EatSleepCoffeeCode/Program.cs
+++ EatSleepCoffeeCode/Program.cs
@@ -4,9 +4,30 @@ namespace EatSleepCoffeeCode
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("|CODING CYCLE| Eat(); Sleep(); Coffee(); Code();\n");
+            Program pizzaSlices = new Program();
+            Console.WriteLine($"\n|SLICES| Number of slices per coding cycle: { pizzaSlices.Eat()}");
+
         }
+
+        public int Eat()
+        {
+            bool alive = true;
+            int numberOfPizzaSlicesEaten = 2;
+
+            while(alive)
+            {
+                Console.WriteLine("Eating Fire on the Mountain Pizza... NOM NOM NOM :o :| :o :| :o :|");
+                alive = false;
+            }
+
+            return numberOfPizzaSlicesEaten;
+
+        }
+
     }
 }
